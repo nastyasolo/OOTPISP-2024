d3538d1d03e3be9e9bb9a0b78be87b2ae5963a30
nastyasolo <125284999+nastyasolo@users.noreply.github.com>
2024-06-02 17:55:31 +0300

    change_

--- trunk/po0_220219/task_05/src/Time.cpp
+++ trunk/po0_220219/task_05/src/Time.cpp
@@ -2,12 +2,7 @@
 Time::Time(void) : min(0), sec(0) {}
 Time::Time(int m, int s) : min(m), sec(s) {}
 Time::Time(const Time& t) : min(t.min), sec(t.sec) {}
-Time& Time::operator=(const Time& t) {
-    if (this == &t) return *this;
-    min = t.min;
-    sec = t.sec;
-    return *this;
-}
+
 
 Time Time::operator*(int k) {
     int totalSeconds = (min * 60 + sec) * k;
@@ -16,13 +11,5 @@ Time Time::operator*(int k) {
     return Time(resultMin, resultSec);
 }
 
-ostream& operator<<(ostream& out, const Time& t) {
-    out << t.min << ":" << t.sec;
-    return out;
-}
 
-istream& operator>>(istream& in, Time& t) {
-    in >> t.min >> t.sec;
-    return in;
-}
 
--- trunk/po0_220219/task_05/src/Time.h
+++ trunk/po0_220219/task_05/src/Time.h
@@ -7,11 +7,23 @@ public:
     Time(void);
     Time(int, int);
     Time(const Time&);
-    Time& operator=(const Time&);
+    Time& operator=(const Time& t) {
+        if (this == &t) return *this;
+        min = t.min;
+        sec = t.sec;
+        return *this;
+    }
     Time operator*(int k);
-    friend ostream& operator<<(ostream& out, const Time&);
-    friend istream& operator>>(istream& in, Time&);
+    friend ostream& operator<<(ostream& out, const Time& t) {
+        out << t.min << ":" << t.sec;
+        return out;
+    }
+    friend istream& operator>>(istream& in, Time& t) {
+        in >> t.min >> t.sec;
+        return in;
+    }
 
 private:
     int min, sec;
 };
+
