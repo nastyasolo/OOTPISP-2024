1dac1039fee9ce1f5ecbafc3af8cde34b3c82f3e
nastyasolo <125284999+nastyasolo@users.noreply.github.com>
2024-06-10 16:38:08 +0300

    Update Vector.h

--- trunk/po0_220219/task_05/src/Vector.h
+++ trunk/po0_220219/task_05/src/Vector.h
@@ -2,84 +2,45 @@
 #define VECTOR_H
 
 #include <iostream>
-#include <memory>
+#include <vector>
 
 template <class T>
 class Vector {
 public:
     Vector(int s, T k);
     explicit Vector(int s);
-    Vector(const Vector<T>& a);
-    Vector(Vector<T>&& a) noexcept;
-    Vector& operator=(const Vector<T>& a);
-    Vector& operator=(Vector<T>&& a) noexcept;
     ~Vector() = default;
 
     T& operator[](int index);
     const T& operator[](int index) const;
     int operator()() const;
-
     Vector operator*(int k) const;
+
     friend std::ostream& operator<<(std::ostream& out, const Vector<T>& v) {
-        for (int i = 0; i < v.size; ++i) {
-            out << v.data[i] << " ";
+        for (const auto& elem : v.data) {
+            out << elem << " ";
         }
         return out;
     }
+
     friend std::istream& operator>>(std::istream& in, Vector<T>& v) {
-        for (int i = 0; i < v.size; ++i) {
-            in >> v.data[i];
+        for (auto& elem : v.data) {
+            in >> elem;
         }
         return in;
     }
 
 private:
     int size;
-    std::unique_ptr<T[]> data;
+    std::vector<T> data;
 };
 
 
 template <class T>
-Vector<T>::Vector(int s, T k) : size(s), data(std::make_unique<T[]>(s)) {
-    for (int i = 0; i < size; ++i) {
-        data[i] = k;
-    }
-}
+Vector<T>::Vector(int s, T k) : size(s), data(s, k) {}
 
 template <class T>
-Vector<T>::Vector(int s) : size(s), data(std::make_unique<T[]>(s)) {}
-
-template <class T>
-Vector<T>::Vector(const Vector<T>& a) : size(a.size), data(std::make_unique<T[]>(a.size)) {
-    for (int i = 0; i < size; ++i) {
-        data[i] = a.data[i];
-    }
-}
-
-template <class T>
-Vector<T>::Vector(Vector<T>&& a) noexcept : size(a.size), data(std::move(a.data)) {
-    a.size = 0;
-}
-
-template <class T>
-Vector<T>& Vector<T>::operator=(const Vector<T>& a) {
-    if (this == &a) return *this;
-    size = a.size;
-    data = std::make_unique<T[]>(a.size);
-    for (int i = 0; i < size; ++i) {
-        data[i] = a.data[i];
-    }
-    return *this;
-}
-
-template <class T>
-Vector<T>& Vector<T>::operator=(Vector<T>&& a) noexcept {
-    if (this == &a) return *this;
-    size = a.size;
-    data = std::move(a.data);
-    a.size = 0;
-    return *this;
-}
+Vector<T>::Vector(int s) : size(s), data(s) {}
 
 template <class T>
 T& Vector<T>::operator[](int index) {
